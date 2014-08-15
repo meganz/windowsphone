b80fe6693901f0abc8f2accdcc423ba03fd645bf
GoedWare <info@goedware.com>
2014-08-15 17:10:55 +0200

    Account creation update

--- MegaApp/MegaApp/Models/ConfirmAccountViewModel.cs
+++ MegaApp/MegaApp/Models/ConfirmAccountViewModel.cs
@@ -6,16 +6,20 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using mega;
+using MegaApp.Classes;
+using MegaApp.Pages;
 using MegaApp.Resources;
+using MegaApp.Services;
 
 namespace MegaApp.Models
 {
-    class ConfirmAccountViewModel: BaseViewModel
+    class ConfirmAccountViewModel: BaseViewModel, MRequestListenerInterface
     {
         private readonly MegaSDK _megaSdk;
 
         public ConfirmAccountViewModel(MegaSDK megaSdk)
         {
+            this.ControlState = true;
             this._megaSdk = megaSdk;
             this.ConfirmAccountCommand = new DelegateCommand(this.ConfirmAccount);
         }
@@ -33,7 +37,7 @@ namespace MegaApp.Models
                         MessageBoxButton.OK);
                 else
                 {
-                    this._megaSdk.fastConfirmAccount(ConfirmCode, this._megaSdk.getBase64PwKey(Password));
+                    this._megaSdk.confirmAccount(ConfirmCode, Password, this);
                 }
             }
         }
@@ -50,6 +54,53 @@ namespace MegaApp.Models
 
         public string ConfirmCode { get; set; }
         public string Password { get; set; }
+
+        #endregion
+
+        #region MRequestListenerInterface
+
+        public void onRequestFinish(MegaSDK api, MRequest request, MError e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                ProgessService.SetProgressIndicator(false);
+              
+                this.ControlState = true;
+
+                if (e.getErrorCode() == MErrorType.API_OK)
+                {
+                    MessageBox.Show(AppMessages.ConfirmAccountSucces, AppMessages.ConfirmAccountSucces_Title, MessageBoxButton.OK);
+                    NavigateService.NavigateTo(typeof(LoginPage), NavigationParameter.Normal);
+                }
+                else
+                    MessageBox.Show(String.Format(AppMessages.ConfirmAccountFailed, e.getErrorString()),
+                        AppMessages.ConfirmAccountFailed_Title, MessageBoxButton.OK);
+            });
+        }
+
+        public void onRequestStart(MegaSDK api, MRequest request)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                this.ControlState = false;
+                ProgessService.SetProgressIndicator(true, AppMessages.ProgressIndicator_ConfirmAccount);
+            });
+        }
+
+        public void onRequestTemporaryError(MegaSDK api, MRequest request, MError e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                ProgessService.SetProgressIndicator(false);
+                MessageBox.Show(String.Format(AppMessages.ConfirmAccountFailed, e.getErrorString()),
+                    AppMessages.ConfirmAccountFailed_Title, MessageBoxButton.OK);
+            });
+        }
+
+        public void onRequestUpdate(MegaSDK api, MRequest request)
+        {
+            // No update status necessary
+        }
 
         #endregion
     }
