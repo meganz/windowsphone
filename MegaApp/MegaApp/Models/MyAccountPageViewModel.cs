d34e5f5f8805d2f1f2603a7ca5f87642bb360c70
GoedWare <info@goedware.com>
2014-09-04 13:17:03 +0200

    update

---
+++ MegaApp/MegaApp/Models/MyAccountPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaApp.Models
+{
+    class MyAccountPageViewModel:BaseViewModel
+    {
+        public MyAccountPageViewModel()
+        {
+            //
+        }
+
+        #region Properties
+
+        private string _userName;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        #endregion
+    }
+}
