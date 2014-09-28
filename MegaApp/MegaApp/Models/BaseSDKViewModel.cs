7e474b88df1f69e8fe392ae696908a35f219d50c
GoedWare <info@goedware.com>
2014-09-28 09:32:29 +0200

    Added missing file

---
+++ MegaApp/MegaApp/Models/BaseSDKViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using mega;
+using MegaApp.Resources;
+
+namespace MegaApp.Models
+{
+    public abstract class BaseSdkViewModel: BaseViewModel
+    {
+        protected readonly MegaSDK MegaSdk;
+
+        protected BaseSdkViewModel(MegaSDK megaSdk)
+        {
+            this.MegaSdk = megaSdk;
+        }
+
+        #region Methods
+
+        protected bool IsUserOnline()
+        {
+            bool isOnline = Convert.ToBoolean(this.MegaSdk.isLoggedIn());
+
+            if (!isOnline)
+                MessageBox.Show(AppMessages.UserNotOnline, AppMessages.UserNotOnline_Title, MessageBoxButton.OK);
+
+            return isOnline;
+        }
+
+        #endregion
+    }
+}
