74dc6d7dbba8d70359913bc17b50a8f476631f6d
Javier Serrano <js@mega.co.nz>
2014-09-29 18:21:17 +0200

    Submodule update featuring:
    Initialization of random number generators
    Functions to send feedback and debug messages to the server:
    - submitFeedback()
    - reportDebugEvent()

---
+++ MegaApp/MegaApp/MegaApi/MEGARandomNumberProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using mega;
+
+namespace MegaApp.MegaApi
+{
+    class MEGARandomNumberProvider : MRandomNumberProvider
+    {
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+
+        public virtual void GenerateRandomBlock(byte[] value)
+        {
+            rngCsp.GetBytes(value);
+        }
+    }
+}
