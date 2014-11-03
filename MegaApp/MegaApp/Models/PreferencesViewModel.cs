89ad9894a1cb205b05447db4a04e23f1cee4c1bb
GoedWare <info@goedware.com>
2014-11-03 15:10:43 +0100

    Commit for update

---
+++ MegaApp/MegaApp/Models/PreferencesViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using mega;
+
+namespace MegaApp.Models
+{
+    class PreferencesViewModel: BaseSdkViewModel
+    {
+        public PreferencesViewModel(MegaSDK megaSdk)
+            : base(megaSdk)
+        {
+            this.ExportMasterKeyCommand = new DelegateCommand(ExportMasterKey);
+        }
+
+        #region Commands
+
+        public ICommand ExportMasterKeyCommand { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        private void ExportMasterKey(object obj)
+        {
+           //
+        }
+
+        #endregion
+    }
+}
