766271afab58eeb18911651b60a4aa80deeb3439
javiergm1983 <jg@mega.co.nz>
2015-02-20 14:50:09 +0100

    Fix API_OVERQUOTA bug

--- MegaApp/common/MegaApi/BaseTransferListener.cs
+++ MegaApp/common/MegaApi/BaseTransferListener.cs
@@ -30,27 +30,27 @@ namespace MegaApp.MegaApi
 
                     case MErrorType.API_EOVERQUOTA:
 
-                        // Stop all upload transfers
-                        if (App.MegaTransfers.Count > 0)
+                        Deployment.Current.Dispatcher.BeginInvoke(() =>
                         {
-                            foreach (var item in App.MegaTransfers)
+                            // Stop all upload transfers
+                            if (App.MegaTransfers.Count > 0)
                             {
-                                var transferItem = (TransferObjectModel)item;
-                                if (transferItem == null) continue;
+                                foreach (var item in App.MegaTransfers)
+                                {
+                                    var transferItem = (TransferObjectModel)item;
+                                    if (transferItem == null) continue;
 
-                                if (transferItem.Type == TransferType.Upload)
-                                    transferItem.CancelTransfer();
+                                    if (transferItem.Type == TransferType.Upload)
+                                        transferItem.CancelTransfer();
+                                }
                             }
-                        }
 
-                        //**************************************************
-                        // TODO: Disable the "camera upload" (when availabe)
-                        //**************************************************
+                            //**************************************************
+                            // TODO: Disable the "camera upload" (when availabe)
+                            //**************************************************
 
-
-                        // User notification message.
-                        Deployment.Current.Dispatcher.BeginInvoke(() => DialogService.ShowOverquotaAlert());
-
+                            DialogService.ShowOverquotaAlert();
+                        });
                         break;
                 }                
             });
