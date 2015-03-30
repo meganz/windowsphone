79d98ab35a1b593b0d7ff783cea607919cdd3109
Javier Gómez <jg@mega.co.nz>
2015-03-30 17:41:30 +0200

    Bug fixing
    - Bug #2088: ArgumentOutOfRangeException – SetViewingRange
    - Bug #2089: COMException – IsCameraTypeSupported
    - Bug #2090: ArgumentException - GetExtension

--- MegaApp/common/Services/ImageService.cs
+++ MegaApp/common/Services/ImageService.cs
@@ -40,28 +40,34 @@ namespace MegaApp.Services
 
         public static bool IsImage(string filename)
         {
-            string extension = Path.GetExtension(filename);
+            try 
+            { 
+                string extension = Path.GetExtension(filename);
 
-            if (extension == null) return false;
-            
-            switch (extension.ToLower())
-            {
-                case ".jpg":
-                case ".jpeg":
-                case ".gif":
-                case ".png":
-                case ".tif":
-                case ".tiff":
-                case ".tga":
-                case ".bmp":
+                if (extension == null) return false;
+
+                switch (extension.ToLower())
                 {
-                    return true;
-                }
-                default:
-                {
-                    return false;
+                    case ".jpg":
+                    case ".jpeg":
+                    case ".gif":
+                    case ".png":
+                    case ".tif":
+                    case ".tiff":
+                    case ".tga":
+                    case ".bmp":
+                        {
+                            return true;
+                        }
+                    default:
+                        {
+                            return false;
+                        }
                 }
             }
+            {
+                return false;
+            }            
         }
 
         public static BitmapImage GetBitmapFromStream(Stream stream, int decodePixelHeight, int decodePixelWidth)
