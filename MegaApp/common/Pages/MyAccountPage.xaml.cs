10bf4aec1662cc1f06ed5be5059a92734c5e314d
GoedWare <info@goedware.com>
2015-05-22 15:29:39 +0200

    Update MyAccountPage to PhoneDrawerLayout page

--- MegaApp/common/Pages/MyAccountPage.xaml.cs
+++ MegaApp/common/Pages/MyAccountPage.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Navigation;
 using MegaApp.Classes;
 using MegaApp.Enums;
 using MegaApp.MegaApi;
@@ -13,15 +10,13 @@ using MegaApp.Models;
 using MegaApp.Resources;
 using MegaApp.Services;
 using MegaApp.UserControls;
-using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-using Microsoft.Phone.Tasks;
 using Telerik.Windows.Controls;
 using GestureEventArgs = System.Windows.Input.GestureEventArgs;
 
 namespace MegaApp.Pages
 {
-    public partial class MyAccountPage : PhoneApplicationPage
+    public partial class MyAccountPage : PhoneDrawerLayoutPage
     {
         private readonly MyAccountPageViewModel _myAccountPageViewModel;
 
@@ -29,19 +24,16 @@ namespace MegaApp.Pages
         {
             _myAccountPageViewModel = new MyAccountPageViewModel(App.MegaSdk, App.AppInformation);
             this.DataContext = _myAccountPageViewModel;
+            
             InitializeComponent();
+            InitializePage(MainDrawerLayout, LstHamburgerMenu, HamburgerMenuItemType.MyAccount);
 
-            // Initialize the hamburger menu / slide in
-            MainDrawerLayout.InitializeDrawerLayout();
-            MainDrawerLayout.DrawerOpened += OnDrawerOpened;
-            MainDrawerLayout.DrawerClosed += OnDrawerClosed;
-
-            SetApplicationBar();
+            SetApplicationBarData();
 
             InteractionEffectManager.AllowedTypes.Add(typeof(RadDataBoundListBoxItem));
         }
 
-        private void SetApplicationBar()
+        private void SetApplicationBarData()
         {
             this.ApplicationBar = (ApplicationBar)Resources["MyAccountMenu"];
 
@@ -49,27 +41,16 @@ namespace MegaApp.Pages
             ((ApplicationBarIconButton)ApplicationBar.Buttons[1]).Text = UiResources.Logout.ToLower();
 
             ((ApplicationBarMenuItem)ApplicationBar.MenuItems[0]).Text = UiResources.ClearCache.ToLower();
-
-            /*((ApplicationBarMenuItem)ApplicationBar.MenuItems[0]).Text = UiResources.ChangePassword.ToLower();
-            ((ApplicationBarMenuItem)ApplicationBar.MenuItems[1]).Text = UiResources.ExportMasterKeyText.ToLower();
-            ((ApplicationBarMenuItem)ApplicationBar.MenuItems[2]).Text = UiResources.ClearCache.ToLower();*/
         }
 
-        protected override void OnBackKeyPress(CancelEventArgs e)
-        {
-            // Check if Hamburger Menu is open in view. If open. First slide out before exit
-            e.Cancel = MainDrawerLayout.CloseIfOpen();
 
-            base.OnBackKeyPress(e);
-        }
-
-        private void OnPieDataBindingComplete(object sender, System.EventArgs e)
+        private void OnPieDataBindingComplete(object sender, EventArgs e)
         {
             // Focus on the first datapoint (= Used space)
             //((PieSeries) sender).DataPoints[0].OffsetFromCenter = 0.05;
         }
 
-        private void OnLogoutClick(object sender, System.EventArgs e)
+        private void OnLogoutClick(object sender, EventArgs e)
         {
             int numPendingTransfers = App.MegaTransfers.Count(t => (t.Status == TransferStatus.Queued ||
                 t.Status == TransferStatus.Downloading || t.Status == TransferStatus.Uploading ||
@@ -92,34 +73,25 @@ namespace MegaApp.Pages
         	_myAccountPageViewModel.Logout();
         }
 
-        private void OnSettingsClick(object sender, System.EventArgs e)
+        private void OnSettingsClick(object sender, EventArgs e)
         {
             NavigateService.NavigateTo(typeof(SettingsPage), NavigationParameter.Normal);
         }
+       
 
-        /*private void OnChangePasswordClick(object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void OnExportMasterkeyClick(object sender, System.EventArgs e)
-        {
-
-        }*/
-
-        private void OnClearCacheClick(object sender, System.EventArgs e)
+        private void OnClearCacheClick(object sender, EventArgs e)
         {
             App.MainPageViewModel.CloudDrive.ChildNodes.Clear();
             App.MainPageViewModel.RubbishBin.ChildNodes.Clear();
             _myAccountPageViewModel.ClearCache();
         }        
 
-        private void OnItemTap(object sender, Telerik.Windows.Controls.ListBoxItemTapEventArgs e)
+        private void OnItemTap(object sender, ListBoxItemTapEventArgs e)
         {
             App.MegaSdk.getPaymentId(((Product)e.Item.DataContext).Handle, new GetPaymentUrlRequestListener());
         }
 
-        private void OnPivotLoaded(object sender, System.Windows.RoutedEventArgs e)
+        private void OnPivotLoaded(object sender, RoutedEventArgs e)
         {
             if (sender == PivotAccount)
                 _myAccountPageViewModel.GetAccountDetails();
@@ -127,46 +99,18 @@ namespace MegaApp.Pages
                 _myAccountPageViewModel.GetPricing();
         }
 
-        private void OnSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             //LstProducts.SelectedItem = null;
         }
 
-        private void OnHamburgerMenuItemTap(object sender, ListBoxItemTapEventArgs e)
+
+        protected override void OnDrawerClosed(object sender)
         {
-            // Needed on every UI interaction
-            App.MegaSdk.retryPendingConnections();
-
-            var hamburgerMenuItem = e.Item.DataContext as HamburgerMenuItem;
-            if (hamburgerMenuItem == null) return;
-
-            if (hamburgerMenuItem.Type == HamburgerMenuItemType.MyAccount)
-                MainDrawerLayout.CloseDrawer();
-            else
-                hamburgerMenuItem.TapAction.Invoke();
-            
-            LstHamburgerMenu.SelectedItem = null;
+            base.OnDrawerClosed(sender);
+            SetApplicationBarData();
         }
-
-        private void OnHamburgerTap(object sender, GestureEventArgs e)
-        {
-            // Needed on every UI interaction
-            App.MegaSdk.retryPendingConnections();
-
-            MainDrawerLayout.OpenDrawer();
-        }
-
-        private void OnDrawerClosed(object sender)
-        {
-            SetApplicationBar();
-        }
-
-        private void OnDrawerOpened(object sender)
-        {
-            // Remove application bar from display when sliding in the hamburger menu
-            this.ApplicationBar = null;
-        }
-
+        
         private void OnMyAccountTap(object sender, GestureEventArgs e)
         {
             // Needed on every UI interaction
@@ -174,5 +118,22 @@ namespace MegaApp.Pages
 
             MainDrawerLayout.CloseDrawer();
         }
+
+        #region Override Events
+
+        // XAML can not bind them direct from the base class
+        // That is why these are dummy event handlers
+
+        protected override void OnHamburgerTap(object sender, GestureEventArgs e)
+        {
+            base.OnHamburgerTap(sender, e);
+        }
+
+        protected override void OnHamburgerMenuItemTap(object sender, ListBoxItemTapEventArgs e)
+        {
+            base.OnHamburgerMenuItemTap(sender, e);
+        }
+
+        #endregion
     }
 }
