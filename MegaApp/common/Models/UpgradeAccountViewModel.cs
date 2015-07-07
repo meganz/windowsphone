dde2602a4000d2e2edb879bc89c5c15d54b60404
Javier Gómez <jg@mega.co.nz>
2015-07-07 14:33:27 +0200

    Cheking active payment methods + Minor fixes
    + Include checking to show the CC payment methods only when they are
    active
    + Minor compilation fixes
    + New images (waiting for assets)

---
+++ MegaApp/common/Models/UpgradeAccountViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MegaApp.Classes;
+
+namespace MegaApp.Models
+{
+    class UpgradeAccountViewModel : BaseViewModel
+    {
+        public UpgradeAccountViewModel()
+        {
+            Plans = new ObservableCollection<ProductBase>();
+            Products = new ObservableCollection<Product>();
+        }
+
+        public ObservableCollection<ProductBase> Plans { get; set; }
+        public ObservableCollection<Product> Products { get; set; }
+
+        private Product _productPurchased;
+        public Product ProductPurchased
+        {
+            get { return _productPurchased; }
+            set
+            {
+                _productPurchased = value;
+                OnPropertyChanged("ProductPurchased");
+            }
+        }
+
+        private bool _creditCardPaymentMethodAvailable;
+        public bool CreditCardPaymentMethodAvailable
+        {
+            get { return _creditCardPaymentMethodAvailable; }
+            set
+            {
+                _creditCardPaymentMethodAvailable = value;
+                OnPropertyChanged("CreditCardPaymentMethodAvailable");
+            }
+        }
+
+        private bool _availablePurchases;
+        public bool AvailablePurchases
+        {
+            get { return _availablePurchases; }
+            set
+            {
+                _availablePurchases = value;
+                OnPropertyChanged("AvailablePurchases");
+            }
+        }
+    }
+}
