6b2554f941bd7e908505e02a7f11bf6f77a8049a
Javier Gómez <jg@mega.co.nz>
2015-08-18 19:18:39 +0200

    Change contacts list controls
    + Manage onUsersUpdate
    + Telerik RadJumpList instead of LongListSelector
    + Contacts list context menu
    + Delete contact and view contact details features

--- MegaApp/common/Models/ContactsViewModel.cs
+++ MegaApp/common/Models/ContactsViewModel.cs
@@ -18,6 +18,7 @@ using MegaApp.Pages;
 using MegaApp.Resources;
 using MegaApp.Services;
 using Microsoft.Phone.Shell;
+using Telerik.Windows.Data;
 
 namespace MegaApp.Models
 {
@@ -36,34 +37,27 @@ namespace MegaApp.Models
             IgnoreRequestCommand = new DelegateCommand(IgnoreRequest);
             DeclineRequestCommand = new DelegateCommand(DeclineRequest);
 
+            ViewContactCommand= new DelegateCommand(ViewContact);
+            DeleteContactCommand= new DelegateCommand(DeleteContact);
+
             UpdateUserData();
             
             InitializeMenu(HamburgerMenuItemType.Contacts);
 
-            MegaContactsSortOrder = ContactSortOrderType.ORDER_ALPHABETICAL_ASC;
+            MegaContactsSortMode = ListSortMode.Ascending;
 
             MegaContactsList = new ObservableCollection<Contact>();
             MegaContactsList.CollectionChanged += MegaContacts_CollectionChanged;
+            
+            CreateContactsGroupDescriptors();
+            CreateContactSortDescriptors();
 
             ReceivedContactRequests = new ObservableCollection<ContactRequest>();
-            SentContactRequests = new ObservableCollection<ContactRequest>();
+            SentContactRequests = new ObservableCollection<ContactRequest>();            
         }
 
         void MegaContacts_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            MegaContactsDataSource = AlphaKeyGroup<Contact>.CreateGroups(MegaContactsList,
-                System.Threading.Thread.CurrentThread.CurrentUICulture,
-                (Contact s) => 
-                {
-                    if (!String.IsNullOrWhiteSpace(s.FirstName))
-                        return s.FirstName;
-                    else if (!String.IsNullOrWhiteSpace(s.LastName))
-                        return s.LastName;
-                    else
-                        return s.Email;
-                }, 
-                true, MegaContactsSortOrder);
-
             OnPropertyChanged("NumberOfMegaContacts");
             OnPropertyChanged("IsMegaContactsListEmpty");
             OnPropertyChanged("NumberOfMegaContactsText");
@@ -87,22 +81,36 @@ namespace MegaApp.Models
         public ICommand AcceptRequestCommand { get; set; }
         public ICommand IgnoreRequestCommand { get; set; }
         public ICommand DeclineRequestCommand { get; set; }
+        
+        public ICommand ViewContactCommand { get; set; }
+        public ICommand DeleteContactCommand { get; set; }
 
         #endregion
 
         #region Properties
 
         public ContactDisplayMode CurrentDisplayMode { get; set; }
-        public ContactSortOrderType MegaContactsSortOrder { get; set; }
-
-        private List<AlphaKeyGroup<Contact>> _megaContactsDataSource;
-        public List<AlphaKeyGroup<Contact>> MegaContactsDataSource
+        public ListSortMode MegaContactsSortMode { get; set; }
+        
+        private ObservableCollection<GenericGroupDescriptor<Contact, String>> groupDescriptors;
+        public ObservableCollection<GenericGroupDescriptor<Contact, String>> GroupDescriptors
         {
-            get { return _megaContactsDataSource; }
+            get { return groupDescriptors; }
             set
             {
-                _megaContactsDataSource = value;
-                OnPropertyChanged("MegaContactsDataSource");
+                groupDescriptors = value;
+                OnPropertyChanged("GroupDescriptors");
+            }
+        }
+
+        private ObservableCollection<GenericSortDescriptor<Contact, String>> sortDescriptors;
+        public ObservableCollection<GenericSortDescriptor<Contact, String>> SortDescriptors
+        {
+            get { return sortDescriptors; }
+            set
+            {
+                sortDescriptors = value;
+                OnPropertyChanged("SortDescriptors");
             }
         }
 
@@ -204,6 +212,30 @@ namespace MegaApp.Models
                 MContactRequestReplyActionType.REPLY_ACTION_DENY, this);
         }
 
+        private void ViewContact(object obj)
+        {
+            this.ViewContactDetails();
+        }
+
+        private void DeleteContact(object obj)
+        {
+            if (FocusedContact != null)
+            {
+                var customMessageDialog = new CustomMessageDialog(
+                    AppMessages.DeleteContactQuestion_Title,
+                    String.Format(AppMessages.DeleteContactQuestion, FocusedContact.Email),
+                    App.AppInformation,
+                    MessageDialogButtons.OkCancel);
+
+                customMessageDialog.OkOrYesButtonTapped += (sender, args) =>
+                {
+                    MegaSdk.removeContact(MegaSdk.getContact(FocusedContact.Email), this);
+                };
+
+                customMessageDialog.ShowDialog();
+            }
+        }        
+
         #endregion
 
         #region Public Methods
@@ -295,8 +327,65 @@ namespace MegaApp.Models
                     // Do nothing. Just exit this background process because a cancellation exception has been thrown
                 }
 
-            }, LoadingCancelToken, TaskCreationOptions.PreferFairness, TaskScheduler.Current);
-        }        
+            }, LoadingCancelToken, TaskCreationOptions.PreferFairness, TaskScheduler.Current);            
+        }
+
+        public void CreateContactsGroupDescriptors()
+        {
+            if(GroupDescriptors == null)
+                GroupDescriptors = new ObservableCollection<GenericGroupDescriptor<Contact, String>>();
+
+            GenericGroupDescriptor<Contact, String> group = new GenericGroupDescriptor<Contact, String>();                        
+            group.SortMode = MegaContactsSortMode;
+            group.KeySelector = (contact) =>
+            {
+                if (!String.IsNullOrWhiteSpace(contact.FirstName))
+                    return contact.FirstName.Substring(0, 1).ToLower();
+                else if (!String.IsNullOrWhiteSpace(contact.LastName))
+                    return contact.LastName.Substring(0, 1).ToLower();
+                else
+                    return contact.Email.Substring(0, 1).ToLower();
+            };
+
+            GroupDescriptors.Add(group);            
+        }
+
+        public void CreateContactSortDescriptors()
+        {
+            if (SortDescriptors == null)
+                SortDescriptors = new ObservableCollection<GenericSortDescriptor<Contact, String>>();
+
+            GenericSortDescriptor<Contact, String> sort = new GenericSortDescriptor<Contact, String>();
+            sort.SortMode = MegaContactsSortMode;
+            sort.KeySelector = (contact) =>
+            {
+                if (!String.IsNullOrWhiteSpace(contact.FirstName))
+                    return contact.FirstName;
+                else if (!String.IsNullOrWhiteSpace(contact.LastName))
+                    return contact.LastName;
+                else
+                    return contact.Email;
+            };
+
+            SortDescriptors.Add(sort);
+        }
+
+        public void SortContacts(ListSortMode sortMode)
+        {
+            if (sortMode != ListSortMode.None && MegaContactsSortMode != sortMode)
+            {
+                MegaContactsSortMode = sortMode;
+
+                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    GroupDescriptors.Clear(); GroupDescriptors = null;
+                    SortDescriptors.Clear(); SortDescriptors = null;
+
+                    CreateContactsGroupDescriptors();
+                    CreateContactSortDescriptors();
+                });                
+            }
+        }
 
         public void GetReceivedContactRequests()
         {
@@ -348,6 +437,15 @@ namespace MegaApp.Models
             inputDialog.ShowDialog();
         }
 
+        public void ViewContactDetails()
+        {
+            if (FocusedContact != null)
+            {
+                PhoneApplicationService.Current.State["SelectedContact"] = FocusedContact;
+                NavigateService.NavigateTo(typeof(ContactDetailsPage), NavigationParameter.Normal);
+            }
+        }
+
         public void ChangeMenu(IList iconButtons, IList menuItems)
         {
             switch (CurrentDisplayMode)
@@ -397,43 +495,88 @@ namespace MegaApp.Models
                 ProgressService.SetProgressIndicator(false);
             });
 
-            if (e.getErrorCode() == MErrorType.API_OK)
+            switch(request.getType())
             {
-                if(request.getType() == MRequestType.TYPE_INVITE_CONTACT)
-                {
-                    switch(request.getNumber())
+                case MRequestType.TYPE_INVITE_CONTACT:
+                    if (e.getErrorCode() == MErrorType.API_OK)
                     {
-                        case (int)MContactRequestInviteActionType.INVITE_ACTION_ADD:
-                        case (int)MContactRequestInviteActionType.INVITE_ACTION_DELETE:
-                        case (int)MContactRequestInviteActionType.INVITE_ACTION_REMIND:                            
-                            break;
-                    }
-                }
-                else if(request.getType() == MRequestType.TYPE_REPLY_CONTACT_REQUEST)
-                {
-                    switch (request.getNumber())
-                    {
-                        case (int)MContactRequestReplyActionType.REPLY_ACTION_ACCEPT:
-                        case (int)MContactRequestReplyActionType.REPLY_ACTION_DENY:
-                        case (int)MContactRequestReplyActionType.REPLY_ACTION_IGNORE:                            
-                            break;
-                    }
-                }
-                else if(request.getType() == MRequestType.TYPE_GET_ATTR_USER)
-                {
-                    foreach(var contactRequest in SentContactRequests)
-                    {
-                        if(contactRequest.Email.Equals(request.getEmail()))
-                            Deployment.Current.Dispatcher.BeginInvoke(() => contactRequest.AvatarUri = new Uri(request.getFile(), UriKind.RelativeOrAbsolute));
-                    }
+                        switch (request.getNumber())
+                        {
+                            case (int)MContactRequestInviteActionType.INVITE_ACTION_ADD:
+                                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                                {
+                                    new CustomMessageDialog(
+                                        AppMessages.InviteContactSuccessfully_Title,
+                                        String.Format(AppMessages.InviteContactSuccessfully, request.getEmail()),
+                                        App.AppInformation,
+                                        MessageDialogButtons.Ok).ShowDialog();
+                                });
+                                break;
 
-                    foreach (var contactRequest in ReceivedContactRequests)
+                            case (int)MContactRequestInviteActionType.INVITE_ACTION_DELETE:
+                            case (int)MContactRequestInviteActionType.INVITE_ACTION_REMIND:
+                                break;
+                        }
+                    }
+                    else
                     {
-                        if (contactRequest.Email.Equals(request.getEmail()))
-                            Deployment.Current.Dispatcher.BeginInvoke(() => contactRequest.AvatarUri = new Uri(request.getFile(), UriKind.RelativeOrAbsolute));
-                    }                    
-                }
-            }
+                        switch (request.getNumber())
+                        {
+                            case (int)MContactRequestInviteActionType.INVITE_ACTION_ADD:
+                                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                                {
+                                    new CustomMessageDialog(
+                                        AppMessages.InviteContactFailed_Title,
+                                        AppMessages.InviteContactFailed,
+                                        App.AppInformation,
+                                        MessageDialogButtons.Ok).ShowDialog();
+                                });
+                                break;
+
+                            case (int)MContactRequestInviteActionType.INVITE_ACTION_DELETE:
+                            case (int)MContactRequestInviteActionType.INVITE_ACTION_REMIND:
+                                break;
+                        }
+                    }
+                    break;
+
+                case MRequestType.TYPE_REPLY_CONTACT_REQUEST:
+                    if (e.getErrorCode() == MErrorType.API_OK)
+                    {
+                        switch (request.getNumber())
+                        {
+                            case (int)MContactRequestReplyActionType.REPLY_ACTION_ACCEPT:
+                            case (int)MContactRequestReplyActionType.REPLY_ACTION_DENY:
+                            case (int)MContactRequestReplyActionType.REPLY_ACTION_IGNORE:
+                                break;
+                        }
+                    }
+                    break;
+
+                case MRequestType.TYPE_GET_ATTR_USER:
+                    if (e.getErrorCode() == MErrorType.API_OK)
+                    {
+                        foreach (var contactRequest in SentContactRequests)
+                        {
+                            if (contactRequest.Email.Equals(request.getEmail()))
+                                Deployment.Current.Dispatcher.BeginInvoke(() => contactRequest.AvatarUri = new Uri(request.getFile(), UriKind.RelativeOrAbsolute));
+                        }
+
+                        foreach (var contactRequest in ReceivedContactRequests)
+                        {
+                            if (contactRequest.Email.Equals(request.getEmail()))
+                                Deployment.Current.Dispatcher.BeginInvoke(() => contactRequest.AvatarUri = new Uri(request.getFile(), UriKind.RelativeOrAbsolute));
+                        }
+                    }
+                    break;
+
+                case MRequestType.TYPE_REMOVE_CONTACT:
+                    if (e.getErrorCode() == MErrorType.API_OK)
+                    {
+
+                    }
+                    break;
+            }            
         }
 
         public virtual void onRequestStart(MegaSDK api, MRequest request)
