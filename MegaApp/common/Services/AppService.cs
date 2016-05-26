e2a8fa33da056ece87d4001545be491092e6b3be
Javier Gómez <jg@mega.co.nz>
2016-05-26 16:00:35 +0200

    New update release
    - Change the version number.
    - SQLite version upgrade.
    - MegaSDK Submodule update.

--- MegaApp/common/Services/AppService.cs
+++ MegaApp/common/Services/AppService.cs
@@ -49,7 +49,7 @@ namespace MegaApp.Services
 
         public static string GetMegaSDK_Version()
         {
-            return String.Format("2070ecc");
+            return String.Format("4364ab7");
         }
 
         public static string GetAppUserAgent()
