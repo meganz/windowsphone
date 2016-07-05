63a02ed22e46f8fe3cf4e2db0f0182b5f1e0f600
Javier Gómez <jg@mega.co.nz>
2016-07-05 17:44:29 +0200

    MegaSDK Submodule Update

--- MegaApp/common/Services/AppService.cs
+++ MegaApp/common/Services/AppService.cs
@@ -49,7 +49,7 @@ namespace MegaApp.Services
 
         public static string GetMegaSDK_Version()
         {
-            return String.Format("4364ab7");
+            return String.Format("eaf07d9");
         }
 
         public static string GetAppUserAgent()
