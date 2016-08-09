db69ff03ec6728e141569ce5816217c5c8ab7976
Javier Gómez <jg@mega.co.nz>
2016-08-09 11:04:48 +0200

    Fix WP8.0 compilation issues

--- MegaApp/common/Services/LicenseService.cs
+++ MegaApp/common/Services/LicenseService.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using Windows.ApplicationModel.Email;
 using Windows.Foundation;
 using Windows.Storage;
 using mega;
